d43acc10a936a854038397a607c06508afff0f70
Morgane Arnaud <morgane@mohara.co>
2025-03-28 09:11:54 +0000

    chore: Clean up

--- constants/Map.constants.ts
+++ constants/Map.constants.ts
@@ -4,5 +4,5 @@ export const USER_CURRENT_LONGITUDE = -121.4346243738888;
 
 // Delta values for the region
 // These values define how much of the map to show around the center point
-export const DEFAULT_LATITUDE_DELTA = 0.536; // Typical value for zooming in to a city-level view
+export const DEFAULT_LATITUDE_DELTA = 0.536; // Value for zooming in to a city-level view
 export const DEFAULT_LONGITUDE_DELTA = 0.374; // Adjust based on your map's zoom level
